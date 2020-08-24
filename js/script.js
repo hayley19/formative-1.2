b5ff0c4d48ec6dc5d469219f2334f57c8068c68d
hayley19 <hayleylisaclarke19@gmail.com>
2020-08-24 14:23:24 +1200

    made single page

---
+++ js/script.js
@@ -0,0 +1,61 @@
+
+
+$(document).ready(function(){
+
+  $('.booking-page').hide();
+  $('.home-page').show();
+  $('.contact-page').hide();
+  $('.menu-page').hide();
+
+
+
+  $('#booking-page').click(function(){
+    $('.booking-page').show();
+    $('.home-page').hide();
+    $('.contact-page').hide();
+    $('.menu-page').hide();
+  });
+
+
+  $('#booking-page-button').click(function(){
+    $('.booking-page').show();
+    $('.home-page').hide();
+    $('.contact-page').hide();
+    $('.menu-page').hide();
+  });
+
+
+  $('#contact-page').click(function(){
+    $('.booking-page').hide();
+    $('.home-page').hide();
+    $('.contact-page').show();
+    $('.menu-page').hide();
+  });
+
+
+  $('#home-page').click(function(){
+    $('.booking-page').hide();
+    $('.home-page').show();
+    $('.contact-page').hide();
+    $('.menu-page').hide();
+  });
+
+
+  $('#home-page-button').click(function(){
+    $('.booking-page').hide();
+    $('.home-page').show();
+    $('.contact-page').hide();
+    $('.menu-page').hide();
+  });
+
+
+  $('#menu-page').click(function() {
+    $('.booking-page').hide();
+    $('.home-page').hide();
+    $('.contact-page').hide();
+    $('.menu-page').show();
+  });
+
+
+
+});
